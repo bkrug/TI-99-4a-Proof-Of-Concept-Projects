048fa2b21f97b680893a54a93f393aa7fc8549e1
Benjamin Krug <benjaminkrug@yahoo.com>
2022-09-11 17:06:37 -0400

    Delete code that has been moved to "envelope-demo" repo

--- Music/CONST.asm
+++
@@ -1,7 +0,0 @@
-*
-* Other
-*
-MINUTE EQU  3600          Length of minute in VDP Interrupts
-VDP1DF EQU  >01E2         Default value for VDP Register 1
-*
-FCTFLG EQU  >80           In a byte this bit implies function key is down
